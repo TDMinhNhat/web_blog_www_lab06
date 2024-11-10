a04e28cfef2cfd9a0dcf60d9498d336aee5ca2b9
MinhNhat <tdminhnhat13092003@gmail.com>
2024-11-10 09:17:27 +0700

    init project

---
+++ src/test/java/dev/skyherobrine/app/WwwLab06ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwLab06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
