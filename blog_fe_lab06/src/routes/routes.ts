02e9f2d4739e858126109aced9f81f28eb773955
MinhNhat <tdminhnhat13092003@gmail.com>
2024-11-10 12:23:59 +0700

    create routes

---
+++ blog_fe_lab06/src/routes/routes.ts
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: null,
+        errorElement: null
+    },
+    {
+        path: "/admin",
+        element: null
+    }
+])
+
+export default routes;
