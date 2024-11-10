a08a13067fe4461309d9dd89fee8e8b1ad0aa942
MinhNhat <tdminhnhat13092003@gmail.com>
2024-11-10 12:39:34 +0700

    create error page

--- blog_fe_lab06/src/routes/routes.ts
+++ blog_fe_lab06/src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../pages/ErrorPage";
+import React from "react";
 
 const routes = createBrowserRouter([
     {
         path: "/",
         element: null,
-        errorElement: null
+        errorElement: React.createElement(ErrorPage, null)
     },
     {
         path: "/admin",
