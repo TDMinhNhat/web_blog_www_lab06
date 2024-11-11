3d6cf9084712f40d5ae0cc747e8c081593910ace
MinhNhat <tdminhnhat13092003@gmail.com>
2024-11-11 09:00:09 +0700

    setting tab active

--- blog_fe_lab06/src/components/admin/TabAdminComponent.tsx
+++ blog_fe_lab06/src/components/admin/TabAdminComponent.tsx
@@ -8,22 +8,40 @@ import CommentIcon from '@mui/icons-material/Comment';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HomeIcon from '@mui/icons-material/Home';
 import LogoutIcon from '@mui/icons-material/Logout';
+import $ from "jquery";
 
-export default function TabAdminComponent() {
+export default function TabAdminComponent({ tab, setTab }: { tab: string, setTab: (tab: string) => void }) {
+
+  React.useEffect(() => {
+    if(tab == "post") {
+      $("#tabPost").addClass("nav-item-active")
+      $("#tabPost span").addClass("nav-item-text-active")
+      $("#tabPostComment, #tabUser").removeClass("nav-item-active")
+    } else if(tab == "post_comment") {
+      $("#tabPostComment").addClass("nav-item-active")
+      $("#tabPostComment span").addClass("nav-item-text-active")
+      $("#tabPost, #tabUser").removeClass("nav-item-active")
+    } else {
+      $("#tabUser").addClass("nav-item-active")
+      $("#tabUser span").addClass("nav-item-text-active")
+      $("#tabPost, #tabPostComment").removeClass("nav-item-active")
+    }
+  }, [tab])
+
   return (
     <div className="h-100 position-fixed top-0 start-0 d-flex flex-column justify-content-between background-dark-color">
       {/* Control about manage */}
       <div>
         <ul className="navbar-nav">
-            <li className="d-flex align-items-center nav-item p-2">
+            <li className="d-flex align-items-center nav-item p-2" onClick={() => setTab("post")} id="tabPost">
               <PostAddIcon sx={{width: 40, height: 40, color: "red" }} />
               <span className="align-text-center ms-2 text-light-color fw-bold">Post</span>
             </li>
-            <li className="d-flex align-items-center nav-item p-2">
+            <li className="d-flex align-items-center nav-item p-2" onClick={() => setTab("post_comment")} id="tabPostComment">
               <CommentIcon sx={{width: 40, height: 40, color: "red" }} />
               <span className="align-text-center ms-2 text-light-color fw-bold">Post Comment</span>
             </li>
-            <li className="d-flex align-items-center nav-item p-2">
+            <li className="d-flex align-items-center nav-item p-2" onClick={() => setTab("user")} id="tabUser">
               <AccountCircleIcon sx={{width: 40, height: 40, color: "red" }} />
               <span className="align-text-center ms-2 text-light-color fw-bold">User</span>
             </li>
--- blog_fe_lab06/src/pages/AdminPage.tsx
+++ blog_fe_lab06/src/pages/AdminPage.tsx
@@ -4,7 +4,10 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.js";
 import TabAdminComponent from "../components/admin/TabAdminComponent.js";
 
 export default function AdminPage() {
+
+    const [tab, setTab] = React.useState("post");
+
     return <div className="container-fluid h-100 d-flex flex-row">
-        <TabAdminComponent />
+        <TabAdminComponent tab={tab} setTab={setTab} />
     </div>  
 }
